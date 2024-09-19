f6e4686bac377ef10b6157e611999affab37d048
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-09-19 21:53:39 +0700

    update

---
+++ bai2-buoi1/Bai2_Buoi1 (1).cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+	printf("Ma lop: DA24TTD.\nTen lop: CNTT.\nMSSV: 110124151.\nHo ten: Nguyen Duy Phuoc.\nNgay thang nam sinh: 25/08/2006.\nDia chi: Ap Dai Thon A, xa Hoa Minh, huyen Chau Thanh. tinh Tra Vinh.\nSo dien thoai: 0369439519");
+
+
+	return 0;
+}
